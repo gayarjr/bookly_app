1127887398f8ee1a8ed1028c12feef1c0f1c90ec
mahmoud ashraf <mahmodashraf@gmail.com>
2024-09-11 15:45:26 +0300

    best seller list view item part 1

--- lib/features/Home/data/presentaion/views/widgets/HomeViewBody.dart
+++ lib/features/Home/data/presentaion/views/widgets/HomeViewBody.dart
@@ -23,7 +23,39 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller ',
             style: Styles.titlemeduim,
-          )
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.white,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetData.testimage),
+                ),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
+          ),
         ],
       ),
     );
