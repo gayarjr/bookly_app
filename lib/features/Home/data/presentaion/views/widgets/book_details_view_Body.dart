16532d570b6d8b033838c3ddfb4b5f318390af11
mahmoud ashraf <mahmodashraf@gmail.com>
2024-09-14 14:57:12 +0300

    add custem scroll view

--- lib/features/Home/data/presentaion/views/widgets/book_details_view_Body.dart
+++ lib/features/Home/data/presentaion/views/widgets/book_details_view_Body.dart
@@ -13,59 +13,73 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          CustemBookdetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .2),
-            child: CustemBookItem(),
-          ),
-          const SizedBox(
-            height: 43,
-          ),
-          Text(
-            'the Jungle Book',
-            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          Opacity(
-            opacity: .7,
-            child: Text(
-              ' hong kong',
-              style: Styles.textStyle18.copyWith(
-                  fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                CustemBookdetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * .2),
+                  child: CustemBookItem(),
+                ),
+                const SizedBox(
+                  height: 43,
+                ),
+                Text(
+                  'the Jungle Book',
+                  style:
+                      Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Opacity(
+                  opacity: .7,
+                  child: Text(
+                    ' hong kong',
+                    style: Styles.textStyle18.copyWith(
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(
+                  height: 57,
+                ),
+                const BooksAction(),
+                const Expanded(
+                  child: const SizedBox(
+                    height: 50,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You Can Olso like ',
+                    style: Styles.textStyle14
+                        .copyWith(fontWeight: FontWeight.w600),
+                  ),
+                ),
+                const SizedBox(
+                  height: 14,
+                ),
+                const simillarBooksListView(),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 18,
-          ),
-          BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 57,
-          ),
-          const BooksAction(),
-          const SizedBox(
-            height: 50,
-          ),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You Can Olso like ',
-              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
-            ),
-          ),
-          const SizedBox(
-            height: 14,
-          ),
-          const simillarBooksListView(),
-        ],
-      ),
+        )
+      ],
     );
+    ;
   }
 }
