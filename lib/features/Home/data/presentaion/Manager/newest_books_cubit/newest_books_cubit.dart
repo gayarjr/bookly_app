65055a8ddeb46df1f691946970fc0b43aec8031a
mahmoud ashraf <mahmodashraf@gmail.com>
2024-09-16 14:22:23 +0300

    news books cubit done

--- lib/features/Home/data/presentaion/Manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/Home/data/presentaion/Manager/newest_books_cubit/newest_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/Home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/Home/data/repo/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsbooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchfeaturedBooks();
+
+    result.fold((falure) {
+      emit(NewestBooksFailure(falure.errorMessage));
+    }, (books) {
+      emit(NewestBooksSucces(books));
+    });
+  }
 }
