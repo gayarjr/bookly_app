ee666e98a1f0940538be767f1fdce3de310dd578
mahmoud ashraf <mahmodashraf@gmail.com>
2024-09-16 13:49:56 +0300

    create feature books method

--- lib/features/Home/data/presentaion/Manager/features_books_cubit/features_books_cubit.dart
+++ lib/features/Home/data/presentaion/Manager/features_books_cubit/features_books_cubit.dart
@@ -1,4 +1,24 @@
 import 'package:bookly_app/features/Home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/Home/data/repo/home_repo.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'features_books_state.dart';
+
+class FeaturesBooksCubit extends Cubit<FeaturesBooksState> {
+  FeaturesBooksCubit(this.homeRepo) : super(FeaturesBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturebooks() async {
+    emit(FeaturesBooksLoading());
+    var result = await homeRepo.fetchfeaturedBooks();
+
+    result.fold((falure) {
+      emit(FeaturesBooksfaluire(falure.errorMessage));
+    }, (books) {
+      emit(FeaturesBooksSucces(books));
+    });
+  }
+}
