18baadb04a1590b779934fd94c808d3a697be89d
mahmoud ashraf <mahmodashraf@gmail.com>
2024-09-20 18:15:39 +0300

    cahched network image

--- lib/features/Home/data/presentaion/views/widgets/custem_list_view_item.dart
+++ lib/features/Home/data/presentaion/views/widgets/custem_list_view_item.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/core/Utls/asset.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustemBookItem extends StatelessWidget {
@@ -7,15 +8,14 @@ class CustemBookItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(imageUrl),
-          ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => Icon(Icons.error),
         ),
       ),
     );
